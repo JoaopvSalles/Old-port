d98066d361d234ae0e21c858e68878d8c55a7d6f
joao <joaopaulovs2000@gmail.com>
2022-08-29 17:27:25 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -41,17 +41,14 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.example.com' > CNAME
 
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:joaopvSalles/Portfolio.git main:gh-pages
 
 cd -
