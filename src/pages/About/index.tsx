ee966c7d2a494c8f0184243d2c43b833163f270c
joao <joaopaulovs2000@gmail.com>
2022-08-29 15:36:55 -0300

    Att-7

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -6,8 +6,8 @@ export function About() {
             <div>
                 <h2><span>&lt;</span> Sobre mim <span>/&gt;</span></h2>
                 <p>
-                    Meu nome é João Paulo sou um apaixonado por tecnologia e estou buscando uma oportunidade para ingressar no mercado de programação.
-                    Estou em constante evolução, estudando e desenvolvendo projetos todos os dias, criei este portfolio com o intuito de demonstrar meus conhecimentos e o que venho aprendendo.
+                    Meu nome é João Paulo sou apaixonado por tecnologia e estou buscando uma oportunidade para ingressar no mercado de programação.
+                    Estou em constante evolução, estudando e desenvolvendo projetos todos os dias, criei este portfólio com o intuito de demonstrar meus conhecimentos e o que venho aprendendo.
                 </p>
                 < br />
                 <p>
